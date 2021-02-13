ccc3d738c60865045464141afc79126193ca3fdf
Adam Mahood <mahood.adam@gmail.com>
2021-02-13 11:20:26 -0500

    UWP-23: creating login api route (#2)

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,15 @@
 const RouteHandler = require("../lib/route_handler/RouteHandler");
 
+const LoginRouteHandler = require("./login");
+
 class APIRouteHandler extends RouteHandler {
   handler() {
+    const loginRoute = new LoginRouteHandler("/login", this.db);
+
+    this.router.use(loginRoute.path, loginRoute.handler());
+
     return this.router;
   }
 }
 
-module.exports = APIRouteHandler
+module.exports = APIRouteHandler;

ea74a13fe16a513889bf822f7f9c7bddcffdd708
d2kalra <deep.m.kalra@gmail.com>
2021-02-17 19:58:34 -0500

    uwp-25: added semesters routes (#5)
    Co-authored-by: d2kalra <d2kalra@uwaterloo.ca>
    
    Closes uwpokerclub/tickets#25

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,6 +3,7 @@ const RouteHandler = require("../lib/route_handler/RouteHandler");
 const LoginRouteHandler = require("./login");
 const UsersRouteHandler = require("./users");
 const EventsRouteHandler = require("./events");
+const SemestersRouteHandler = require("./semesters");
 
 const { requireAuthentication } = require("../middleware/authenticate");
 
@@ -11,10 +12,12 @@ class APIRouteHandler extends RouteHandler {
     const loginRoute = new LoginRouteHandler("/login", this.db);
     const usersRoute = new UsersRouteHandler("/users", this.db);
     const eventsRoute = new EventsRouteHandler("/events", this.db);
+    const semestersRoute = new SemestersRouteHandler("/semesters", this.db)
 
     this.router.use(loginRoute.path, loginRoute.handler());
     this.router.use(usersRoute.path, requireAuthentication, usersRoute.handler());
     this.router.use(eventsRoute.path, requireAuthentication, eventsRoute.handler());
+    this.router.use(semestersRoute.path, requireAuthentication, semestersRoute.handler());
 
     return this.router;
   }
---
+++ src/routes/semesters.js
@@ -0,0 +1,119 @@
+/* eslint-disable camelcase */
+const RouteHandler = require("../lib/route_handler/RouteHandler");
+const { CODES, EVENT_STATE } = require("../models/constants");
+
+function validateCreateReq(body) {
+  const { name, startDate, endDate, meta } = body;
+
+  const nonNullValues = [name, startDate, endDate];
+
+  if (nonNullValues.some((v) => v === undefined)) {
+      throw new Error("Missing required fields: name, startDate, format, semesterId");
+  }
+
+  if (typeof(name) !== "string") {
+      throw new Error("Required fields have incorrect type: name");
+  }
+  
+  if (meta !== undefined && typeof(meta) !== "string") {
+    throw new Error("Optional fields have incorrect type: meta");
+  }
+
+  if (Number.isNaN(Date.parse(startDate))) {
+      throw new Error("Required date fields cannot be parsed: startDate");
+  }
+
+  if (Number.isNaN(Date.parse(endDate))) {
+      throw new Error("Required date fields cannot be parsed: endDate");
+  }
+
+  if (name === "") {
+      throw new Error("Required fields cannot be empty: name");
+  }
+}
+
+class SemestersRouteHandler extends RouteHandler {
+  handler() {
+    this.router.get("/", async (req, res, next) => {
+      
+      const semesters = await this.db
+        .table("semesters")
+        .select()
+        .execute()
+        .catch((err) => {
+          res.status(CODES.INTERNAL_SERVER_ERROR).json({
+            error: "DATABASE_ERROR",
+            message: "A lookup error occurred"
+          });
+
+          next(err);
+        });
+
+      return res.status(CODES.OK).json({ semesters });
+    });
+
+    this.router.get("/:id", async (req, res, next) => {
+      const { id } = req.params;
+
+      const [ semester ] = await this.db
+        .table("semesters")
+        .select()
+        .where("id = ?", id)
+        .execute()
+        .catch((err) => {
+          res.status(CODES.INTERNAL_SERVER_ERROR).json({
+            error: "DATABASE_ERROR",
+            message: "A lookup error occurred"
+          });
+
+          next(err);
+        });
+
+      if (semester === undefined) {
+        return res.status(CODES.NOT_FOUND).json({
+          error: "NOT_FOUND",
+          message: "That semester could not be found"
+        });
+      }
+
+      return res.status(CODES.OK).json({ semester });
+    });
+
+    this.router.post("/", async (req, res, next) => {
+      try {
+        validateCreateReq(req.body);
+      } catch (err) {
+        return res.status(CODES.INVALID_REQUEST).json({
+          error: "INVALID_REQUEST",
+          message: err.message
+        });
+      }
+
+      const { name, startDate, endDate, meta } = req.body;
+
+      const semester = await this.db
+        .table("semesters")
+        .create({
+          name,
+          start_date: startDate,
+          end_date: endDate,
+          meta: meta
+        })
+        .execute()
+        .catch((err) => {
+          res.status(CODES.INTERNAL_SERVER_ERROR).json({
+            error: "DATABASE_ERROR",
+            message: "An insertion error occurred"
+          });
+
+          next(err);
+        });
+
+      return res.status(CODES.CREATED).json({ semester });
+    });
+
+    return this.router;
+  }
+}
+
+module.exports = SemestersRouteHandler;

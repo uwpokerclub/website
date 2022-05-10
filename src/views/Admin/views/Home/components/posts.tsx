28605fe1d2a17f791149c50cae3442edcc072d6e
Adam Mahood <mahood.adam@gmail.com>
2022-05-10 10:31:04 -0400

    release/v2.0.0

--- src/views/Admin/views/Home/components/posts.tsx
+++ src/views/Admin/views/Home/components/posts.tsx
@@ -2,6 +2,56 @@ import React from "react";
 
 const blogPosts = [
   {
+    title: "Spring 2022",
+    date: "January 11th, 2022",
+    body: (
+      <>
+        <p>
+          Welcome to the Spring 2022 term. In this update we have added a new budget tracking feature. This allows
+          the executive team to have an accurate count of the amount of money earned throughout a semester. If you
+          have questions or find issues with any features please contact a club admin who can forward the message
+          off to us. Our email also is <b>uwpokerclub@gmail.com</b>.
+        </p>
+
+        <ul className="blog-list">
+          <li>
+            Added more configuartion options when creating a semester. New values include the starting budget
+            of the term, the cost of a membership, the cost of a discounted membership, and the cost of a rebuy.
+          </li>
+
+          <li>
+            The semester dashboard now displays configuration information at the top of the page, as well as the
+            current budget for the term.
+          </li>
+
+          <li>
+            When registering a membership, there is now an additional checkbox to indicate the membership is discounted.
+          </li>
+
+          <li>
+            Additionally, a new button has been added to toggle a member as discounted.
+          </li>
+
+          <li>
+            Added a new table to track various transactions made with club money. Enter negative amounts to
+            represent the club purchasing something, and positive amounts to represent the club receiving money
+            (i.e from WUSA).
+          </li>
+
+          <li>
+            On the event page, a member's number of rebuys is now being tracked, with a button to increase the count
+            by one.
+          </li>
+
+          <li>
+            All of these actions will update the current budget of the semester on the fly, so you will
+            always have an accurate account of how much money is in the club.
+          </li>
+        </ul>
+      </>
+    )
+  },
+  {
     title: "Winter 2020",
     date: "January 4th, 2020",
     body: (

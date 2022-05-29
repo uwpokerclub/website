1fb888068154eee9368efaf02403000f3babb07f
Adam Mahood <mahood.adam@gmail.com>
2022-05-28 21:38:35 -0400

    tickets/127: fixing hamburger menu on mobile (#311)

--- src/shared/components/Navbar/Navbar.tsx
+++ src/shared/components/Navbar/Navbar.tsx
@@ -25,8 +25,8 @@ export default function Navbar(): ReactElement {
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"

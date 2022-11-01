fb93a53478c059de2a24f6f029082ec968f1916e
Adam Mahood <mahood.adam@gmail.com>
2022-11-01 00:02:41 -0400

    devops/181: debugging

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,6 +2,7 @@
 
 if [ $1 = "--test" ]; then
   shift
+  echo "goose -dir ./migrations postgres $TEST_DATABASE_URL?sslmode=disable $@"
   goose -dir ./migrations postgres $TEST_DATABASE_URL?sslmode=disable $@
 else
   goose -dir ./migrations postgres $DATABASE_URL?sslmode=disable $@

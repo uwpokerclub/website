9d1951a1dc90f4e7a4905844f5d6598a18f3b5b3
Adam Mahood <mahood.adam@gmail.com>
2022-12-13 15:59:41 -0500

    testing

--- src/shared/utils/sendAPIRequest.ts
+++ src/shared/utils/sendAPIRequest.ts
@@ -3,21 +3,14 @@ const sendAPIRequest = async <T>(path: string, method = "GET", body?: any): Prom
   const apiUrl = process.env.NODE_ENV === "development" ? "/api" : "https://api.uwpokerclub.com"
 
   const res = await fetch(`${apiUrl}/${path}`, {
-    credentials: "same-origin",
+    credentials: "include",
     method,
     headers: body !== undefined ? { "Content-Type": "application/json" } : undefined,
     body: body !== undefined ? JSON.stringify(body) : undefined
-  })
+  });
 
   // Save response status for later.
   const status = res.status;
-
-  // Check if response header has 'Set-Cookie'
-  const cookie = res.headers.get('set-cookie');
-  if (cookie) {
-    console.log("we are setting a cookie");
-    document.cookie = cookie;
-  }
 
   // Attempt to convert response body to JSON. If this fails that means there is no body.
   let data: T | null = null;

67f6e647bf1e3b0c5abb25537ccc1278aee95c7c
Adam Mahood <mahood.adam@gmail.com>
2023-09-12 14:50:17 -0400

    feat(navbar): implement navbar for public site

--- src/views/Main/components/Main.tsx
+++ src/views/Main/components/Main.tsx
@@ -3,27 +3,16 @@ import { Route, Routes } from "react-router-dom";
 
 import "../main.scss"
 
-import Header from "./Header";
-
-// import LandingPage from "../views/LandingPage";
 import Join from "../views/Join";
 import { Navigate, useLocation } from "react-router-dom";
-
-/*********************************
- *   TODO
- * 
- * /join:
- * -make full <li> items into link
- * 
- * Header:
- **********************************/
+import ResponsiveNavbar from "../../../shared/components/ResponsiveNavbar/ResponsiveNavbar";
 
 function Main(): ReactElement {
   const location = useLocation();
 
   return (
     <>
-      <Header />
+      <ResponsiveNavbar />
       <Routes>
         <Route path="/*" element={<Navigate to="/join" state={{ from: location }} replace />} />
         <Route path="/join" element={<Join />} />

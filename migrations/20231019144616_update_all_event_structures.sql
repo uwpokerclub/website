8488fb6ea1d9914fc88802987b5161c51407cf32
Adam Mahood <mahood.adam@gmail.com>
2023-10-20 17:05:17 -0400

    chore(migrations): improve migration to not error on database initalization

--- migrations/20231019144616_update_all_event_structures.sql
+++ migrations/20231019144616_update_all_event_structures.sql
@@ -1,5 +1,16 @@
 -- +goose Up
 -- +goose StatementBegin
+WITH count AS (
+  SELECT COUNT(*) AS cnt FROM events WHERE structure_id IS NULL
+)
+
+INSERT INTO structures (id, name)
+SELECT 1, 'Dummy Structure'
+WHERE (SELECT cnt FROM count) > 0
+AND NOT EXISTS (
+  SELECT 1 FROM structures WHERE id = 1
+);
+
 UPDATE events SET structure_id = 1 WHERE structure_id IS NULL;
 -- +goose StatementEnd
 

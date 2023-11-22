9ba2bcb9804f06d5cd20a095a09e631c70412aaf
Adam Mahood <mahood.adam@gmail.com>
2023-11-22 12:19:29 -0500

    refactor(vite): implement Join page

--- vite-refactor/src/assets/index.ts
+++ vite-refactor/src/assets/index.ts
@@ -1,3 +1,7 @@
 import jsLogoBlack from "./jsLogoBlack.png";
+import discordLine from "./discord_line.svg";
+import emailLine from "./email_line.svg";
+import facebookLine from "./facebook_line.svg";
+import instagramLine from "./instagram_line.svg";
 
-export { jsLogoBlack };
+export { jsLogoBlack, discordLine, emailLine, facebookLine, instagramLine };
---
+++ vite-refactor/src/features/socials-list/components/SocialsList.tsx
@@ -0,0 +1,69 @@
+import { discordLine, instagramLine, facebookLine, emailLine } from "../../../assets";
+import styles from "./SocialsList.module.css";
+
+export function SocialsList() {
+  return (
+    <div className={`jumbotron jumbotron-fluid vertical-center p-0 m-0 ${styles.bgImage}`}>
+      {/* <img src={clo th} className="img-fluid" /> */}
+      <div className={`container ${styles.verticalCenter}`}>
+        <div className="row w-100">
+          <div className="col-lg">
+            {/* <span className="m-4">
+              <b>Location:</b> MC2054
+              <br/>
+              First time playing? <a className="text-link" href="/register"><b>Register here!</b></a>
+            </span> */}
+          </div>
+          <div className="col-lg">
+            <ul className={styles.socialsList}>
+              {/* <li>
+                <a className="nav-link" href=""></a>
+              </li> */}
+              {/*<li>
+              <a className="form-link" href="https://forms.gle/p7hB6XbTPfsdijrE6" target="_blank" rel="noreferrer">
+              <img src={formsLine} className="mr-4 pl-1 pr-2" alt=""/>
+                  2023 WINTER MIDTERM
+                </a>
+            </li>*/}
+              <li>
+                <a className={styles.textLink} href="https://discord.gg/2k4h9sM" target="_blank" rel="noreferrer">
+                  <img src={discordLine} className="mr-4" alt="" />
+                  JOIN THE DISCORD
+                </a>
+              </li>
+              <li>
+                <a
+                  className={styles.textLink}
+                  href="https://www.instagram.com/uwpokerclub/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={instagramLine} className="mr-4" alt="" />
+                  FOLLOW ON INSTAGRAM
+                </a>
+              </li>
+              <li>
+                <a
+                  className={styles.textLink}
+                  href="https://www.facebook.com/uwpokerstudies"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={facebookLine} className="mr-4 pl-1 pr-2" alt="" />
+                  LIKE US ON FACEBOOK
+                </a>
+              </li>
+              <li>
+                <a className={styles.textLink} href="mailto:uwaterloopoker@gmail.com" target="_blank" rel="noreferrer">
+                  <img src={emailLine} className="mr-4" alt="" />
+                  CONTACT US VIA EMAIL
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg"></div>
+        </div>
+      </div>
+    </div>
+  );
+}

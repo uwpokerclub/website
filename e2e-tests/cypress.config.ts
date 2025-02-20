eb8c697a8d908131d56f9e906c26f5ad06041ba6
Adam Mahood <mahood.adam@gmail.com>
2025-02-20 16:14:23 -0500

    build(e2e): add cypress recording to e2e tests

--- e2e-tests/cypress.config.ts
+++ e2e-tests/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "rxir4a",
   e2e: {
     baseUrl: "http://localhost:5173",
     // setupNodeEvents(on, config) {

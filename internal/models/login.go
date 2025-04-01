b3aad629940a2e2afc35a8e27bf4557150d0f103
Adam Mahood <mahood.adam@gmail.com>
2025-04-01 17:04:35 -0400

    fix(model): add gorm default to role on login model

--- internal/models/login.go
+++ internal/models/login.go
@@ -3,5 +3,5 @@ package models
 type Login struct {
 	Username string `json:"username" binding:"required" gorm:"primaryKey"`
 	Password string `json:"password" binding:"required"`
-	Role     string
+	Role     string `gorm:"default:executive"`
 }
---
+++ internal/services/login_service_test.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"api/internal/database"
+	"api/internal/models"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoginService(t *testing.T) {
+	t.Setenv("ENVIRONMENT", "TEST")
+
+	db, err := database.OpenTestConnection()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer sqlDB.Close()
+
+	wipeDB := func() {
+		err := database.WipeDB(db)
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	}
+
+	loginService := NewLoginService(db)
+
+	t.Run("CreateLogin", func(t *testing.T) {
+		t.Cleanup(wipeDB)
+
+		err := loginService.CreateLogin("testuser", "testpassword")
+		if assert.NoError(t, err) {
+			var login models.Login
+			err := db.First(&login, "username = ?", "testuser").Error
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+			assert.Equal(t, "testuser", login.Username)
+			assert.NotEmpty(t, login.Password)
+			assert.Equal(t, "executive", login.Role)
+		}
+	})
+}
